86ad8bade4a95d2c2781c583ef55ea9629f668f5
David Rees <d.w.rees.03@gmail.com>
2017-03-25 09:33:15 +0000

    firstcommit

---
+++ js/config.js
@@ -0,0 +1,6 @@
+"use strict"
+var subscribeUrl = "http://daveconservatoire.us3.list-manage.com/subscribe?u=13b1689b6dc75646a2d866525&id=c3d6f26708";
+var email = "david@graphqlforbeginners.com";
+var countdown = false;
+var configDate = "2018 01 01";
+var particles = true;
