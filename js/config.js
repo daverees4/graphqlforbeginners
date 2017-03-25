1f9b5b4854c782b0351f09de44088607dc95ea58
David Rees <d.w.rees.03@gmail.com>
2017-03-25 09:49:37 +0000

    fix signup

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 "use strict"
-var subscribeUrl = "http://daveconservatoire.us3.list-manage.com/subscribe?u=13b1689b6dc75646a2d866525&id=c3d6f26708";
-var email = "david@graphqlforbeginners.com";
+var subscribeUrl = "http://daveconservatoire.us3.list-manage.com/subscribe/post?u=13b1689b6dc75646a2d866525&id=c3d6f26708";
+var email = "d.w.rees.03@gmail.com";
 var countdown = false;
 var configDate = "2018 01 01";
 var particles = true;
